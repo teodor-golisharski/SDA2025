6e1245d06fe074ae5e9d79165bd9ef0f8e1372e4
Teodor Golisharski <t.golisharski@gmail.com>
2025-10-21 11:32:25 +0300

    Section Sorting Algorithms added. Bubble Sort is implemented

---
+++ SortingAlgorithms/BubbleSort.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+
+// Bubble Sort - slow sorting algorithm
+// Time Complexity = O(n^2)
+
+// Bubble Sort using std::vector
+void bubbleSort(std::vector<int> arr)
+{
+	bool swapped;
+
+	for (int i = 0; i < arr.size() - 1; i++)
+	{
+		swapped = false;
+		for (int j = 0; j < arr.size() - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				std::swap(arr[j], arr[j + 1]);
+				swapped = true;
+			}
+		}
+
+		if (!swapped)
+		{
+			break;
+		}
+	}
+}
+
+// Bubble Sort using standard array
+void swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void bubbleSort(int arr[], int n)
+{
+	bool swapped;
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		swapped = false;
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			swap(&arr[j], &arr[j + 1]);
+		}
+
+		if (!swapped)
+		{
+			break;
+		}
+	}
+}
